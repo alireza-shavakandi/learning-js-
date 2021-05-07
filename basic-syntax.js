67a20908feff30ed7755ac3475f94cfb3665b973
alireza-shavakandi <82979057+alireza-shavakandi@users.noreply.github.com>
2021-05-08 02:20:01 +0430

    Add files via upload

---
+++ basic-syntax.js
@@ -0,0 +1,36 @@
+//***********variables**************//
+var b = "alireza";
+console.log(b);
+var SumNumber=22222;
+console.log(SumNumber)
+//*******changing Html through js********//
+document.getElementById('someText').innerHTML=('HI')
+// getting input 
+        //var age = prompt("what is your age ?")
+        //console.log("your age is ", age)
+//umbers in java script 
+var num=12 
+num=num+1
+var num2=13
+num2++
+console.log("num2 is ",num2)
+console.log("the remaider of  num2 out of 6 is ",num2%6)
+console.log(num2+=10)
+//*********functions in javascript*******//
+/* 1=creation 
+2=calling
+*/
+function aleert(){
+    alert("a function is being run")
+}
+
+//aleert();
+// make a grreting fuction
+function greeting(namme){
+
+    let c= "hello "+ namme;
+    console.log(c)
+
+}
+//var esm = prompt("what's your name?");
+//greeting(esm)
