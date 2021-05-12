66826d7feb930dda9506f7a1674d8569674729f3
alireza-shavakandi <82979057+alireza-shavakandi@users.noreply.github.com>
2021-05-13 03:35:23 +0430

    Add files via upload

--- Smallest multiple.js
+++ Smallest multiple.js
@@ -1,22 +1,40 @@
-// Lcm (Largest common multiple)of 4 & 6 & 9: (2^2) & (3*2)&(3^2) = 36
-/*
-for 1:20 
-find the primes 
-count the number primes factor for each and get the max number of prime factor 
-multiply max powers
+// method 1 prime factorization
+function IsPrime(num){
+    prime=true;
+    for(i=2;i<=Math.sqrt(num);i++){
+        if(num%i==0){
+            prime=false;
+            break;
+        }
+    }
+    return prime;
+}
+console.time('Execution Time');
 
-*/
-// function IsPrime(num){
-//     prime=true;
-//     for(i=2;i<=Math.sqrt(num);i++){
-//         if(num%i==0){
-//             prime=false;
-//             break;
-//         }
-//     }
-//     return prime;
+primelist=new Array();
+for(let i=2;i<=20;i++){
+        if(IsPrime(i)){
+        primelist.push(i);
+    }
+}
+ans=1
+n=1;
+for(i=0;i<primelist.length;i++){
+    while(Math.pow(primelist[i],n)<=20){
+        
+       ans1=Math.pow(primelist[i],n)
+       //this is for debugging //console.log('primelist[i]:',primelist[i],'n:',n,'ans1:',ans1)
+        n++
+    }
+    n=1
+    ans*=ans1
+}
+console.log(ans)
+console.timeEnd('Execution Time');
 
-// }
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
+/*method 2 the lcm and gcd relation
+
 function gcdcalc(a,b){
     let gcd =1;
 
@@ -36,8 +54,10 @@ function lcmcalc(a,b){
 }
 a=1;
 b=2;
-while(b<=10){
+console.time('Execution Time')
+while(b<=30){
     a=lcmcalc(a,b)
     b++
 }
 console.log(a)
+console.timeEnd('Execution Time')*/
